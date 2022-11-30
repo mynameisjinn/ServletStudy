e18b95ceff3daa1cd8a8227cda42a994850cc3fb
hyejin <obndo8539@naver.com>
2022-11-30 21:50:11 +0900

    GET,POST

---
+++ ServletStudy/src/main/java/com/study/servlet/form/FormAPI1.java
@@ -0,0 +1,43 @@
+package com.study.servlet.form;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/api/form/1")
+public class FormAPI1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get 요청");	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("post 요청");
+		
+		request.setCharacterEncoding("UTF-8");
+		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("phone"));
+		System.out.println(request.getParameter("email"));
+		System.out.println(request.getParameter("address"));
+		System.out.println(request.getParameter("pw"));
+		
+		response.setContentType("text/plain; charset=utf8");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("name : "+ request.getParameter("name"));
+		out.println("phone : "+ request.getParameter("phone"));
+		out.println("email : "+ request.getParameter("email"));
+		out.println("address : "+ request.getParameter("address"));
+		out.println("pw : "+ request.getParameter("pw"));
+	}
+
+}
