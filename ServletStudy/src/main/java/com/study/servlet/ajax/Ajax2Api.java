7012a94de7be7ede86ad23f729ed864d6c877641
hyejin <obndo8539@naver.com>
2022-12-06 21:49:57 +0900

    JSON 형변환

--- ServletStudy/src/main/java/com/study/servlet/ajax/Ajax2Api.java
+++ ServletStudy/src/main/java/com/study/servlet/ajax/Ajax2Api.java
@@ -1,6 +1,7 @@
 package com.study.servlet.ajax;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 import java.util.Map;
 
@@ -10,23 +11,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.study.util.DTO;
+
+class Address{
+	private String address1;
+	private String address2;
+	private String address3;
+}
 
 @WebServlet("/api/ajax2")
 public class Ajax2Api extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Ajax2 post 요청");
+//		System.out.println("Ajax2 post 요청");
 		
-		Map<String, String> requestMap = DTO.getParams(request);
+//		Map<String, String> requestMap = DTO.getParams(request);
+		String jsonData = request.getParameter("jsonData");
+		
+		System.out.println(jsonData);
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		Map<String, Object> addressMap = gson.fromJson(jsonData, Map.class);
+		
+		Address address = gson.fromJson(jsonData, Address.class);
+		System.out.println(addressMap.get("address1"));
+		System.out.println(address);
+		
+		Address address2 = Address.builder()
+				.address1("부산광역시")
+				.address2("부산 진구")
+				.address3("삼정타워")
+				.build();
+		
+		String responseJson = gson.toJson(address2);
+		
+//		System.out.println(addressMap);
 
 		response.setContentType("text/plain; charser=utf8");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
-		
-		out.print(requestMap.get("address1")+" "+requestMap.get("address2")+" "+
-										requestMap.get("address3"));
+		out.print(responseJson);
+//		out.print(requestMap.get("address1")+" "+requestMap.get("address2")+" "+
+//										requestMap.get("address3"));
 	}
 
 }
